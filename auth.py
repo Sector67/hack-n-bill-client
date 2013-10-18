db3b9781014008034cc7449e65ce8139a01ad9a3
Peter Novotnak <pnovotnak@flexion.us>
2013-10-17 20:24:53 -0500

    initial commit

---
+++ auth.py
@@ -0,0 +1,23 @@
+import requests
+
+from settings import servers
+
+
+master = servers['master']
+master_login = master['login']
+
+'''
+Takes nothing, returns a bound requests session object
+'''
+def log_in():
+    login_url = master['url'] + master['login']['uri']
+    data = {
+        master_login['username_form_key']: master_login['username'],
+        master_login['password_form_key']: master_login['password'],
+        'form_id': master_login['login_form_id'],
+    }
+    s = requests.session()
+    r = s.post(login_url, data=data)
+    return r
+
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+# Global settings
+
+
+try:
+    from local_settings import *
+except:
+    print "Failed to import local settings!"
+
