b59cf870b9545d80648299410738e89400688a1d
Peter Novotnak <pnovotnak@flexion.us>
2013-10-17 22:10:19 -0500

    updated settings template for local server

--- local_settings-template.py
+++ local_settings-template.py
@@ -2,8 +2,24 @@
 
 # Servers config
 servers = {
+    # Master server
     'master': {
         'url': 'http://drupalinstall.example.org',
+        'login': {
+            # Username and password
+            'username': 'frontdoor',
+            'password': 'frontdoorpass',
+
+            # These might change with Drupal versions
+            'uri': '/user/login',
+            'username_form_key': 'name',
+            'password_form_key': 'pass',
+            'login_form_id': 'user_login',
+        },
+    },
+    # Local server that actually does auth
+    'local': {
+        'url': 'http://localhost',
         'login': {
             # Username and password
             'username': 'frontdoor',
@@ -17,5 +33,4 @@ servers = {
         }
     }
 }
-
 
